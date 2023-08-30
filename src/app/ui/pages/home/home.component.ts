216982a1dac22668c1900ceceb304887ad75bfbe
richard.liao <richard.liao@tpisoftware.com>
2023-08-30 10:33:35 +0800

    chore: 專案模板

--- src/app/ui/pages/home/home.component.ts
+++ src/app/ui/pages/home/home.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { appVersion } from '@app/constants';
 @Component({
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public version = appVersion;
+
+  constructor() {}
+}
